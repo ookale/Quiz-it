15a4b03d78078978aa10523cd1230d2694b82518
ookale <ookale@gmail.com>
2022-03-26 14:12:26 +0000

    Add buildQuiz function to js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,35 @@ const resultContainer = document.getElementById("results");
 const submitButton = document.getElementById("submit");
 const startButton = document.getElementById("start-btn");
 
-function buildQuiz(){}
+function buildQuiz(){
+    const output = [];
+
+    questions.forEach(
+      (currentQuestion. questionNumber) => {
+
+          const answers = [];
+
+          for(letter in currentQuestion.answers){
+              answers.push(
+                `<label>
+                    <input type="radio" name="question${questionNumber}" value="${letter}">
+                        ${letter} :
+                        ${currentQuestion.answers[letter]}
+                </label>`
+              );
+          }
+
+          output.push(
+            `<div class="question"> ${currentQuestion.question} </div>
+             <div class="answers"> ${answers.join('')} </div>`
+          );
+      }
+    );
+    quizContainer.innerHTML = output.join('');
+}
+
+
+
 function showResults(){}
 function startGame(){}
 
@@ -60,7 +88,5 @@ const questions = [
         correctAnswer: "c"  
     },  
     
-
-
-]
+];
 
